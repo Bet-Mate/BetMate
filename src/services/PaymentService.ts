df1bfdad7ba3fa27975f03f7a2b7248c9eb09bd1
Zaiidmo <Zaiidmoumnii@gmail.com>
2025-03-23 20:12:52 +0000

    As a user I can now withdraw through an intuitive UI/UX

--- src/services/PaymentService.ts
+++ src/services/PaymentService.ts
@@ -74,8 +74,39 @@ export const verifyPayment = async (transactionId?: string) => {
         Authorization: `Bearer ${token}`,
       },
     });
-    
+
     return response.data;
+  } catch (err: any) {
+    if (err instanceof AxiosError) {
+      console.error("Axios Error: ", err.response?.data || err.message);
+    } else {
+      console.error("Unknown Error: ", err);
+    }
+    throw err;
+  }
+};
+
+export const withdraw = async (amount: number, withdrawalMethod: string) => {
+  try {
+    const storedAuth = localStorage.getItem("auth");
+    const token = storedAuth ? JSON.parse(storedAuth).accessToken : null;
+    if (!token) {
+      throw new Error("No authentication token found");
+    }
+
+    const response = await apiClient.post(
+      "/payment/withdraw",
+      {
+        amount,
+        withdrawalMethod,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data
   } catch (err: any) {
     if (err instanceof AxiosError) {
       console.error("Axios Error: ", err.response?.data || err.message);
